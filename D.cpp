87ff548589ede75350ec7b7fec86e8237ea6ee7a
Maria Eduarda <m.eduardaferreira05@gmail.com>
2024-10-27 05:37:19 -0300

    Criação da Classe D

---
+++ D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class D {
+private:
+    int D1;
+    float D2;
+
+public:
+    D(int d1, float d2) : D1(d1), D2(d2) {}
+
+    void MD1() {
+        cout << "MD1" << endl;
+    }
+
+    void MD2() {
+        cout << "MD2" << endl;
+    }
+}
