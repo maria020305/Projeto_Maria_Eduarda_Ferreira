57a126e7d809ef36c11123f59c9e239286e5d269
Maria Eduarda <m.eduardaferreira05@gmail.com>
2024-10-27 04:47:25 -0300

    Criação da Classe C

---
+++ C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class C {
+public:
+    string C1;
+    int C2;
+
+    void setC1(string value) { C1 = value; }
+    string getC1() { return C1; }
+
+    void setC2(int value) { C2 = value; }
+    int getC2() { return C2; }
+
+    void MC1() { 
+        cout << "MC1" << endl; 
+    }
+    
+    void MC2() { 
+        cout << "MC2" << endl; 
+    }
+};
